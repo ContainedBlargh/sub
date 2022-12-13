c455b97ce7ab3c734fecdd1262b9006e409de281
ContainedBlargh <jon@zendata.dk>
2022-12-13 23:07:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Sub
+
+> Substitutes strings
+
+Ever thought that `sed` and `awk` were sedulously awkward to use? Ever wondered how ASCII art might look if we could give it some color?
+
+Well, wonder no more!
+
+
+## Usage
+
+```
+Usage:
+  sub [OPTIONS] REPLACEMENT STRING SUBSTITUTION STRING
+
+Replace characters, regular expressions or colors read from stdin.
+
+Positional arguments:
+  replacement string    The string to use for replacing.
+  substitution string   The string to substitute with.
+
+Optional arguments:
+  -h,--help             Show this help message and exit
+  -r,--regex            Interpret argument as regular expression.
+  -c,--capture          Inject captured strings into substitute using &n where
+                        n is a group number.
+  -f,--fg FG            Set a new foreground color.
+  -b,--bg BG            Set a new background color.
+```
+
+## Example - Merry Christmas
+
+Ah, just in time for Christmas, I've found a christmas tree online.
+
+```
+         v
+        >X<
+         A
+        d$b
+      .d\$$b.
+    .d$i$$\$$b.
+       d$$@b
+      d\$$$ib
+    .d$$$\$$$b
+  .d$$@$$$$\$$ib.
+      d$$i$$b
+     d\$$$$@$b
+  .d$@i$\$$i$$@b.
+.d$i$$$$$$\$$$$$$b.
+        ###
+        ###
+        ###
+```
+
+But, it looks rather dull, does it not?
+
+Well, with a little help from `sub`, we can give it some colors!
+
+```sh
+cat .\xmas_tree.txt | sub -f '#F4F556' i i | sub -f "#009302" -rc '([\.bd$])' '&1' | sub -f '#FFC300' -rc '([v<>XA])' '&1' | sub -f "#733719" '#' '#' | sub -f "FF5533" '@' '@' | sub -f "#0F73D9" '\' '\'
+```
+
+Let's break it down a bit:
+
+- `cat` outputs our tree to stdout.
+- Each call to `sub` reads every line from stdin and writes the modified lines to stdout.
+- `sub -f '#F4F556' i i` sets [a foreground color](https://www.color-hex.com/color/f4f556) to each of the `i`-characters (the candles) in our tree. It replaces each i with another i, but applies the foreground color too.
+- `sub -f "#009302" -rc '([\.bd$])' '&1'` matches all the characters in the pattern `[\.bd$]`, determined by the `-r` flag. It also captures all the characters in the pattern, determined by the `-c` flag and the parentheses outside the pattern. Then, it replaces the matched characters with ... themselves, since `'&1'` refers to the first group captured.
+
+The rest of the calls use the same flags with different characters and colors.
+
+![The final result](christmas_tree.png)
+
+## Requirements
+
+A true-color terminal. Microsoft added the Windows Terminal, which you can get on the Windows store.
+If you are running Linux, most terminals should support true-color.
+
+## Installation
+
+You can download a build for Windows or Linux from the releases page.
---
+++ src/main.rs
@@ -0,0 +1,163 @@
+use std::io::{stdin, BufRead};
+
+use argparse::{ArgumentParser, Store, StoreOption, StoreTrue};
+use colored::{ColoredString, Colorize};
+use regex::{Captures, Match, Regex};
+
+fn colorize(fg: Option<String>, bg: Option<String>) -> impl Fn(&str) -> ColoredString {
+    let rgb_re = Regex::new("#?([\\da-fA-F]+)").unwrap();
+    let fg = if let Some(fg) = fg {
+        let captures = rgb_re.captures(&fg).unwrap();
+        let m = captures.get(1).unwrap();
+        let rgb = hex::decode(m.as_str()).unwrap();
+        Some(rgb)
+    } else {
+        None
+    };
+    let bg = if let Some(bg) = bg {
+        let captures = rgb_re.captures(&bg).unwrap();
+        let m = captures.get(1).unwrap();
+        let rgb = hex::decode(m.as_str()).unwrap();
+        Some(rgb)
+    } else {
+        None
+    };
+    move |s: &str| {
+        let with_fg = if let Some(fg) = fg.clone() {
+            s.truecolor(fg[0], fg[1], fg[2])
+        } else {
+            s.normal()
+        };
+        if let Some(bg) = bg.clone() {
+            with_fg.on_truecolor(bg[0], bg[1], bg[2])
+        } else {
+            with_fg
+        }
+    }
+}
+
+fn main() {
+    colored::control::set_override(true);
+    let mut pattern = false;
+    let mut capture = false;
+    let mut rep_string = String::new();
+    let mut sub_string = String::new();
+    let mut fg_color_hex: Option<String> = None;
+    let mut bg_color_hex: Option<String> = None;
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Replace characters, regular expressions or colors read from stdin.");
+        ap.refer(&mut pattern).add_option(
+            &["-r", "--regex"],
+            StoreTrue,
+            "Interpret argument as regular expression.",
+        );
+        ap.refer(&mut capture).add_option(
+            &["-c", "--capture"],
+            StoreTrue,
+            "Inject captured strings into substitute using &n where n is a group number.",
+        );
+        ap.refer(&mut fg_color_hex).add_option(
+            &["-f", "--fg"],
+            StoreOption,
+            "Set a new foreground color.",
+        );
+        ap.refer(&mut bg_color_hex).add_option(
+            &["-b", "--bg"],
+            StoreOption,
+            "Set a new background color.",
+        );
+        ap.refer(&mut rep_string)
+            .add_argument(
+                "replacement string",
+                Store,
+                "The string to use for replacing.",
+            )
+            .required();
+        ap.refer(&mut sub_string)
+            .add_argument(
+                "substitution string",
+                Store,
+                "The string to substitute with.",
+            )
+            .required();
+        ap.parse_args_or_exit();
+    }
+    let colorize = colorize(fg_color_hex, bg_color_hex);
+    if capture && !sub_string.contains("&") {
+        eprintln!("{}", "Warning: received flag for capturing, but no capture groups (&#) are present in substitution string.");
+    }
+    if capture && (!sub_string.contains("(") || sub_string.contains(")")) {
+        eprintln!("{}", "Warning: received flag for capturing, but no captures are present in replacement string.");
+    }
+    if pattern && capture {
+        let replace_re = Regex::new(&rep_string).unwrap();
+        for line in stdin().lock().lines() {
+            let line = line.unwrap();
+            let cloned_line = line.clone();
+            let parts: Vec<&str> = replace_re.split(&cloned_line).collect();
+            if parts.len() == 1 {
+                println!("{}", line.trim_end());
+                continue;
+            }
+            let mut captures: Vec<Captures> = replace_re.captures_iter(&line).into_iter().collect();
+            captures.reverse();
+            for i in 0..parts.len() - 1 {
+                let part_captures: Option<Vec<Match>> = captures
+                    .pop()
+                    .map(|cs| cs.iter().map(|it| it.unwrap()).collect());
+                let mut raw_sub = sub_string.clone();
+                if let Some(captures) = part_captures {
+                    for i in 0..captures.len() {
+                        let index = format!("&{}", i);
+                        raw_sub = raw_sub.replace(index.as_str(), captures[i].as_str());
+                    }
+                }
+                let sub_colorized = colorize(&raw_sub);
+                print!("{}", format!("{}{}", parts[i], sub_colorized));
+            }
+            let last_capture: Option<Vec<Match>> = captures
+                .pop()
+                .map(|cs| cs.iter().map(|it| it.unwrap()).collect());
+            let mut raw_sub = sub_string.clone();
+            if let Some(captures) = last_capture {
+                for i in 0..captures.len() {
+                    let index = format!("&{}", i);
+                    raw_sub = raw_sub.replace(index.as_str(), captures[i].as_str());
+                }
+                let sub_colorized = colorize(&raw_sub);
+                print!("{}", sub_colorized);
+            }
+            println!("{}", parts[parts.len() - 1].trim_end())
+        }
+    } else if pattern {
+        let replace_re = Regex::new(&rep_string).unwrap();
+        for line in stdin().lock().lines() {
+            let line = line.unwrap();
+            let sub_colorized = colorize(&sub_string);
+            let parts: Vec<&str> = replace_re.split(&line).collect();
+            if parts.len() == 1 {
+                println!("{}", line.trim_end());
+                continue;
+            }
+            for i in 0..parts.len() - 1 {
+                print!("{}", format!("{}{}", parts[i], sub_colorized));
+            }
+            println!("{}", parts[parts.len() - 1].trim_end());
+        }
+    } else {
+        for line in stdin().lock().lines() {
+            let line = line.unwrap();
+            let sub_colorized = colorize(&sub_string);
+            let parts: Vec<&str> = line.split(&rep_string).collect();
+            if parts.len() == 1 {
+                println!("{}", line.trim_end());
+                continue;
+            }
+            for i in 0..parts.len() - 1 {
+                print!("{}", format!("{}{}", parts[i], sub_colorized));
+            }
+            println!("{}", parts[parts.len() - 1].trim_end());
+        }
+    }
+}
